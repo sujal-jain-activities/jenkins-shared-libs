9d1c275ff18aaa2e0dc98ec26a7640856cca8024
Sujal jain <jainsujal139@gmail.com>
2026-01-09 21:27:45 +0530

    Fix indentation and cleanup Docker build script

--- vars/Docker_build.groovy
+++ vars/Docker_build.groovy
@@ -7,8 +7,8 @@ def call(String Projectname,String Imagetag,String credentialsId){
                         )
                         ]){
                 sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                        }
                 sh "docker image tag ${Projectname}:${Imagetag} ${env.dockerHubUser}/${Projectname}:${Imagetag}"
                 sh "docker push ${env.dockerHubUser}/${Projectname}:${Imagetag}"
-                        }
                 echo 'Image Uploaded to DockerHub'
 }
