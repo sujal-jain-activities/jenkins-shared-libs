da6410e919fdd5475c203b5e7b46c1c4345e682d
Sujal jain <jainsujal139@gmail.com>
2026-01-09 20:43:29 +0530

    Creating dockerHub uploading

---
+++ vars/Docker_build.groovy
@@ -0,0 +1,14 @@
+def call(String Projectname,String Imagetag,String credentialsId){
+  withCredentials([
+      usernamePassword(
+                        'credentialsId':'${credentialsId}',
+                        passwordVariable:"dockerHubPass",
+                        usernameVariable:"dockerHubUser"
+                        )
+                        ]){
+                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                sh "docker image tag ${Projectname}:${Imagetag} ${env.dockerHubUser}/${Projectname}:${Imagetag}"
+                sh "docker push ${env.dockerHubUser}/${Projectname}:${Imagetag}"
+                        }
+                echo 'Image Uploaded to DockerHub'
+}
